9e7359a796e2f0932f79d789f485adcfe2adaf73
MarcDeJonge <marc.de.jonge1@telenet.be>
2017-07-03 11:03:10 +0200

    CallableStatement stored procedures

--- src/jdbctheorie/JdbcTheorie.java
+++ src/jdbctheorie/JdbcTheorie.java
@@ -1,8 +1,8 @@
 package jdbctheorie;
 
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -16,23 +16,23 @@ public class JdbcTheorie {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String SELECT_LEVERANCIERS_VAN_EEN_WOONPLAATS = "select naam from leveranciers where woonplaats = ?";
+    private static final String CALL_PLANTEN_MET_EEN_WOORD = "{call PlantenMetEenWoord(?)}";
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Woonplaats:");
-            String woonplaats = scanner.nextLine();
+            System.out.print("Woord:");
+            String woord = scanner.nextLine();
             try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                    PreparedStatement statement = connection.prepareStatement(SELECT_LEVERANCIERS_VAN_EEN_WOONPLAATS)) {
-                statement.setString(1, woonplaats);
+                    CallableStatement statement = connection.prepareCall(CALL_PLANTEN_MET_EEN_WOORD)) {
+                statement.setString(1, '%' + woord + '%');
                 try (ResultSet resultSet = statement.executeQuery()) {
                     while (resultSet.next()) {
                         System.out.println(resultSet.getString("naam"));
                     }
                 }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
         }
     }
 }
